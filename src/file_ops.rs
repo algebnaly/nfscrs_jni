8c285f7997ebe413468a8d963fbb692513d65399
algebnaly <mail@algebnaly.com>
2025-11-08 23:39:44 +0800

    1. finish closeFile 2. wip: setFileTimes

--- src/file_ops.rs
+++ src/file_ops.rs
@@ -1,5 +1,6 @@
+use jni::sys::jboolean;
 use nfscrs::nfscrs_types::AbsolutePath;
-use nfscrs::{NFSClientSession, OpenedFile};
+use nfscrs::{NFSCRSError, NFSClientSession, OpenOptions, OpenedFile};
 
 use jni::JNIEnv;
 use jni::objects::{JByteArray, JString, JValue};
@@ -9,7 +10,7 @@ use jni::{
 };
 
 use crate::basic_attr_bitmap;
-use crate::error::{NfscrsJniError, handle_error, throw_nfs_error};
+use crate::error::{NfscrsJniError, handle_error};
 use crate::file_utils::int_to_open_options;
 
 const NFS4_FILE_READ_RESULT_CLASS_NAME: &str = "com/algebnaly/nfs4c/NFS4FileReadResult";
@@ -102,13 +103,14 @@ fn write_file(
     let limit = env
         .call_method(byte_buffer, "limit", "()I", &[])
         .and_then(|v| v.i())?;
-    
+
     if limit <= 0 {
         let nfs4_file_write_result_class = env.find_class(NFS4_FILE_WRITE_RESULT_CLASS_NAME)?;
-        let result_obj = env.new_object(nfs4_file_write_result_class, "(I)V", &[JValue::from(0)])?;
+        let result_obj =
+            env.new_object(nfs4_file_write_result_class, "(I)V", &[JValue::from(0)])?;
         return Ok(result_obj.into_raw());
     }
-    
+
     let slice: Vec<u8>;
     let slice_ref: &[u8] = if let Ok(addr) = env.get_direct_buffer_address(byte_buffer.into()) {
         // 直接引用 DirectBuffer 数据
@@ -117,12 +119,9 @@ fn write_file(
         // fallback：普通 Heap ByteBuffer
         slice = {
             let mut v = vec![0u8; limit as usize];
-            let byte_array_obj = env
-                .call_method(byte_buffer, "array", "()[B", &[])?
-                .l()?;
-            let v_i8: &mut [i8] = unsafe {
-                std::slice::from_raw_parts_mut(v.as_mut_ptr() as *mut i8, v.len())
-            };
+            let byte_array_obj = env.call_method(byte_buffer, "array", "()[B", &[])?.l()?;
+            let v_i8: &mut [i8] =
+                unsafe { std::slice::from_raw_parts_mut(v.as_mut_ptr() as *mut i8, v.len()) };
             let byte_array = JByteArray::from(byte_array_obj);
             env.get_byte_array_region(byte_array, 0, v_i8)?;
             v
@@ -156,8 +155,12 @@ pub extern "system" fn Java_com_algebnaly_nfs4c_NFS4CNativeBridge_fileClose(
 
     let opened_file_ptr = opened_file as *mut OpenedFile;
     let opened_file_ref: &mut OpenedFile = unsafe { &mut *opened_file_ptr };
-    match close_file(session_ref, opened_file_ref, &mut env) {
-        Ok(_r) => {}
+    match close_file(session_ref, opened_file_ref) {
+        Ok(_r) => {
+            unsafe {
+                let _ = Box::from_raw(opened_file_ptr); // release opened file
+            }
+        }
         Err(e) => {
             handle_error(&mut env, &e);
         }
@@ -165,11 +168,11 @@ pub extern "system" fn Java_com_algebnaly_nfs4c_NFS4CNativeBridge_fileClose(
 }
 
 fn close_file(
-    _session_ref: &mut NFSClientSession,
-    _opened_file_ref: &mut OpenedFile,
-    _env: &mut JNIEnv,
+    session_ref: &mut NFSClientSession,
+    opened_file_ref: &mut OpenedFile,
 ) -> Result<(), NfscrsJniError> {
-    todo!()
+    session_ref.close(opened_file_ref)?;
+    Ok(())
 }
 
 #[allow(non_snake_case)]
@@ -202,7 +205,6 @@ fn get_file_size_from_opened_file(
     crate::attr_utils::get_file_size(&fattr4).map(|size| size as i64)
 }
 
-
 #[allow(non_snake_case)]
 #[unsafe(no_mangle)]
 pub extern "system" fn Java_com_algebnaly_nfs4c_NFS4CNativeBridge_openFile(
@@ -212,35 +214,66 @@ pub extern "system" fn Java_com_algebnaly_nfs4c_NFS4CNativeBridge_openFile(
     path: JString,
     open_options: jint,
 ) -> jlong {
-    let path_str: String = match env.get_string(&path) {
-        Ok(s) => s.into(),
+    let session_ptr = session as *mut NFSClientSession;
+    let session_ref: &mut NFSClientSession = unsafe { &mut *session_ptr };
+    let opts = int_to_open_options(open_options);
+    match open_file(session_ref, &mut env, &path, opts) {
+        Ok(r) => r,
         Err(e) => {
-            let _ = env.throw_new("java/io/IOException", format!("Invalid path: {e}"));
+            handle_error(&mut env, &e);
             return 0;
         }
-    };
+    }
+}
 
-    let abs_path = match AbsolutePath::try_from(path_str) {
-        Ok(p) => p,
-        Err(e) => {
-            let _ = env.throw_new("java/io/IOException", format!("not absolute path: {e}"));
-            return 0;
-        }
-    };
+fn open_file(
+    session_ref: &mut NFSClientSession,
+    env: &mut JNIEnv,
+    path: &JString,
+    opts: OpenOptions,
+) -> Result<jlong, NfscrsJniError> {
+    let path_str: String = env.get_string(&path)?.into();
+    let abs_path = AbsolutePath::try_from(path_str).map_err(|e| NFSCRSError::InnerError(e))?;
+    let opened_file = session_ref.open_file_and_comfirm(&abs_path, opts)?;
+    let file = Box::new(opened_file);
+    let file_ptr = Box::into_raw(file);
+    Ok(file_ptr as jlong)
+}
 
+#[allow(non_snake_case)]
+#[unsafe(no_mangle)]
+pub extern "system" fn Java_com_algebnaly_nfs4c_NFS4CNativeBridge_setFileTimes(
+    mut env: JNIEnv,
+    _this: JObject,
+    session: jlong,
+    path: JString,
+    mtime: jlong,
+    atime: jlong,
+    ctime: jlong,
+    bitmap: jint,
+) -> jboolean {
     let session_ptr = session as *mut NFSClientSession;
     let session_ref: &mut NFSClientSession = unsafe { &mut *session_ptr };
 
-    let opts = int_to_open_options(open_options);
-
-    let opened_file = match session_ref.open_file_and_comfirm(&abs_path, opts) {
-        Ok(r) => r,
+    match set_file_times(session_ref, &mut env, &path, mtime, atime, ctime, bitmap) {
+        Ok(r) => r as jboolean,
         Err(e) => {
-            throw_nfs_error(&mut env, &e);
+            handle_error(&mut env, &e);
             return 0;
         }
-    };
-    let file = Box::new(opened_file);
-    let file_ptr = Box::into_raw(file);
-    file_ptr as jlong
+    }
+}
+
+fn set_file_times(
+    _session_ref: &mut NFSClientSession,
+    env: &mut JNIEnv,
+    path: &JString,
+    _mtime: jlong,
+    _atime: jlong,
+    _ctime: jlong,
+    _bitmap: jint,
+) -> Result<jlong, NfscrsJniError> {
+    let path_str: String = env.get_string(&path)?.into();
+    let _abs_path = AbsolutePath::try_from(path_str).map_err(|e| NFSCRSError::InnerError(e))?;
+    todo!()
 }
